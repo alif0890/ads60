e4e2973550a75bf1de3606e04c5b0a939fd251f0
alif0890 <101611640+alif0890@users.noreply.github.com>
2024-02-17 01:33:52 +0700

    Create 468.js

---
+++ 468.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '7670c517cfaa9f795f090a39c77903d1',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//dilutegulpedshirt.com/7670c517cfaa9f795f090a39c77903d1/invoke.js"></scr' + 'ipt>');
+</script>
